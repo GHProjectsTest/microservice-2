410cef7ce35bf7844b282aadb356dce610bc1dc1
GHProjectsUser <0c96800fee6667fd46ac6f9250dca4040d3038ab>
2020-08-16 08:04:00 +0000

    Init model: GET /songs

---
+++ db/2_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  commedit
+-- Automatically generated sql script for the service 2, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table songs.
+--
+CREATE TABLE commedit.songs (
+  title VARCHAR(255) ,
+CONSTRAINT title_PK PRIMARY KEY (title)
+ 
+);
+
+
+
