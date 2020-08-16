3a1ed2a74b41965eca5ffdb83b2119ee1657b855
GHProjectsUser <mail@rwth>
2020-08-16 08:05:05 +0000

    Http Response[songsResponse] edited by Philipp Dolif

--- src/main/i5/las2peer/services/musicService/MusicService.java
+++ src/main/i5/las2peer/services/musicService/MusicService.java
@@ -113,26 +113,25 @@ public class MusicService extends RESTService {
   public Response getSongs() {
 
 
+    Connection connection;
 
+    try {
+        connection = dbm.getConnection();
+    
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM songs;");
+        ResultSet result = statement.executeQuery();
 
-     
-    // service method invocations
-
-     
-
-
-
-
-    // songsResponse
-    boolean songsResponse_condition = true;
-    if(songsResponse_condition) {
-      JSONObject songsResult = new JSONObject();
-
-      
-
-      return Response.status(HttpURLConnection.HTTP_OK).entity(songsResult.toJSONString()).build();
+        JSONArray a = new JSONArray();
+        while(result.next()) {
+            JSONObject songJson = new JSONObject();
+            songJson.put("title", result.getString("title"));
+            a.add(songJson);
+        }
+        statement.close();
+        return Response.ok(a.toJSONString()).build();
+    } catch (SQLException e) {
+        return Response.serverError().build();
     }
-    return null;
   }
 
 
